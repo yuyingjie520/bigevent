98748d2e253d283445c29281029076b817e98ed8
yuyingjie <abc7473537@qq.com>
2021-08-14 16:16:43 +0800

    m

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,5 +1,37 @@
 $(function() {
     $.ajaxPrefilter(function(options) {
         options.url = 'http://www.liulongbin.top:3008' + options.url
+
+
+
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+
+        }
+
+        options.complete = function(res) {
+            console.log(res);
+            if (res.responseJSON.code === 1 && res.responseJSON.message === '身份验证失败！') {
+                localStorage.removeItem('token');
+                location.href = 'login.html'
+            }
+
+        }
+
+
+
+
+
+
+
     })
+
+
+
+
+
+
+
 })
